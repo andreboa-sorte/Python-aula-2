dcabaaa3137dca27685435d663595e1f9164a2e4
andreboa-sorte <offesmecheplayer@gmail.com>
2019-08-13 19:26:02 -0300

    receiro commite

--- ex12.py
+++ ex12.py
@@ -1,2 +1,18 @@
 #12 - Ler dois valores
 # (considere que não serão lidos valores iguais) e escrever o maior deles.
+
+num2=0
+num1=0
+repetido=True
+while repetido:
+    num1=int(input("digite um numero a ser comparado: "))
+    num2=int(input("digite um numero a ser comparado e que não seja repetido: "))
+    if num1!=num2:
+        repetido=False
+    else:
+        print("numero igual, repita de novo\n")
+
+if num1>num2:
+    print("o numero maior é o: ",num1,", o menor é o: ",num2)
+elif num2>num1:
+    print("o numero maior é o: ",num2,", o menor é o: ",num1)
