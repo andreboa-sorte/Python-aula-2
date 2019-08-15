ea5ba67327bffd6e971745cf8f0a1d547fd41ea0
andreboa-sorte <offesmecheplayer@gmail.com>
2019-08-15 19:18:32 -0300

    'exe14'

---
+++ ex14.py
@@ -0,0 +1,15 @@
+#14 - Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10).
+
+
+
+a=8*1
+b=8*2
+c=8*3
+d=8*4
+e=8*5
+f=8*6
+g=8*7
+h=8*8
+i=8*9
+j=8*10
+print('tabuada do 8:\n8x1:',a,"\n8x2:",b,"\n8x3:",c,"\n8x4:",d,"\n8x5:",e,"\n8x6:",f,"\n8x7:",g,"\n8x8:",h,"\n8x9:",i,"\n8x10:",j,"\n")
