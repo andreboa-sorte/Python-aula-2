0ca5ef3646b9ece3d3d6cb378daaed075eec1f51
andreboa-sorte <offesmecheplayer@gmail.com>
2019-08-26 20:18:15 -0300

    'temporarios'

---
+++ ex21.py
@@ -0,0 +1,20 @@
+'''21 - Crie um classe Funcionário com os atributos nome, idade e salário.
+ Deve ter  um método aumentaSalario. Crie duas subclasses da classe funcionário,
+ programador  e  analista, implementando o método abstrato nas duas subclasses.
+ Para o programador some ao atributo salário mais 20 e ao analista some ao salário mais 30,  mostrando na tela o valor. Depois disso, crie uma classe de testes instanciando os objetos programador e analista e ch
+Crie uma classe chamada Ingresso que possui um valor em reais e um método
+imprimeValor().
+
+a. crie uma classe VIP, que herda Ingresso e possui um valor adicional. Crie um
+método que retorne o valor do ingresso VIP (com o adicional incluído).
+
+b. crie uma classe Normal, que herda Ingresso e possui um método que imprime:
+"Ingresso Normal".
+
+c. crie uma classe CamaroteInferior (que possui a localização do ingresso e métodos
+
+para acessar e imprimir esta localização) e uma classe CamaroteSuperior, que é
+mais cara (possui valor adicional). Esta última possui um método para retornar o
+valor do ingresso. Ambas as classes herdam a classe VIPame o método  aumentaSalario de
+ cada um.
+'''
---
+++ ex22.py
@@ -0,0 +1,17 @@
+'''22 - Crie a classe Imóvel, que possui um endereço e um preço.
+
+a. crie uma classe Novo, que herda Imóvel e possui um adicional no preço. Crie
+métodos de acesso e impressão deste valor adicional.
+
+b. crie uma classe Velho, que herda Imóvel e possui um desconto no preço. Crie
+métodos de acesso e impressão para este desconto.
+'''
+
+class Imovel():
+    def info_imovel(self):
+        endereco=""
+        preco=0.0
+
+
+class NovoImovel(Imovel):
+    def info_imovel(self):
