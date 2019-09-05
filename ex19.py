8a535d47193edaae324c976c8021cfda0422ba24
andreboa-sorte <offesmecheplayer@gmail.com>
2019-09-05 19:47:59 -0300

    'ex19'

--- ex19.py
+++ ex19.py
@@ -1,17 +1,17 @@
 '''
-19 - Crie uma classe chamada Bicicleta. Ela terá os seguintes métodos: quantidadeMarchas(); tipoFreio() e marca();
- Crie também duas classes que estendem esta classe, uma se chamará BicicletaPasseio e a outra BicicletaProfissional.
-  Para ﬁnalizar crie uma classe onde deverá estar o método main e crie uma instancia de cada tipo de bicicleta e
-   mostre o resultado dos métodos.
+19 - Crie uma classe chamada Bicicleta. Ela terá os seguintes métodos: quantidadeMarchas();
+ tipoFreio() e marca(); Crie também duas classes que estendem esta classe,
+ uma se chamará BicicletaPasseio e a outra BicicletaProfissional.
+  Para ﬁnalizar crie uma classe onde deverá estar o método main e crie uma instancia de
+   cada tipo de bicicleta e mostre o resultado dos métodos.
 '''
 class Bicicleta():
     def quantidade_marchas(self):
-
-
+        pass
     def tipo_freio(self):
-
+        pass
     def marca(self):
-
+        pass
 
 class BicicletaPasseio(Bicicleta):
 
@@ -37,4 +37,31 @@ class BicicletaProfissional(Bicicleta):
         print("Marca: Caloi ")
 
 
+class Main():
+    menu=True
+    while menu:
+        op=int(input("1- ver bicicleta de passeio\n"
+                     "2- ver bicicreta profisional\n"
+                     "3- sair\n"
+                     "opção: "))
+        if op==1:
+            chama=BicicletaPasseio()
 
+            print("\nas especificações da bicicleta de passeio é: ")
+
+            chama.marca()
+            chama.quantidade_marchas()
+            chama.tipo_freio()
+
+        elif op==2:
+            chama=BicicletaProfissional()
+
+            print("\nas especificações da bicicleta profissional é: ")
+
+            chama.marca()
+            chama.quantidade_marchas()
+            chama.tipo_freio()
+
+
+        elif op==3:
+            menu=False
