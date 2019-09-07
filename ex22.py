947be9680c9ac73cf160267702028867c48995c7
andreboa-sorte <offesmecheplayer@gmail.com>
2019-09-07 17:29:41 -0300

    'ex21'

--- ex22.py
+++ ex22.py
@@ -9,12 +9,55 @@ métodos de acesso e impressão para este desconto.
 
 class Imovel():
     def info_imovel(self):
-        endereco=" SC-401, 9301 - Santo Antonio de Lisboa, Florianópolis - SC, 88050-001"
-        preco=2000000.0
+       pass
 
 
 class NovoImovel(Imovel):
-    def info_novo_imovel(self,preco):
+    def info_imovel(self):
+        endereco = " SC-401, 9301 - Santo Antonio de Lisboa, Florianópolis - SC, 88050-001"
+        preco = 2000000.0
+
+
         novo_preco=float(input("digite o preço adicional do novo imovel acima do preço de {0:.2f}: ".format(preco)))
 
-        novo_preco=novo_preco + preco
+        novo_preco= preco + novo_preco
+
+        print(" novo imovel\n"
+              "preço: {0:.2f} \n"
+              "endereço: {1}".format(novo_preco,endereco))
+
+class AntigoImovel(Imovel):
+    def info_imovel(self):
+        endereco = " Ed.Zigurate Centro, Rua Saldanha Marinho, 374 - Centro, Florianópolis - SC, 88020-050"
+        preco = 2000000.0
+
+
+        novo_preco = float(input("digite o desconto do imovel antigo acima do preço de {0:.2f}: ".format(preco)))
+
+        novo_preco = preco - novo_preco
+
+        print(" Antigo imovel\n"
+              "preço: {0:.2f} \n"
+              "endereço: {1}".format(novo_preco, endereco))
+
+
+menu=True
+
+while menu:
+    op=int(input("1- novo imovel\n"
+                 "2- antigo movel\n"
+                 "3- sair\n"
+                 "opção: "))
+
+    if op==1:
+        chama=NovoImovel()
+
+        chama.info_imovel()
+
+    elif op==2:
+        chama=AntigoImovel()
+
+        chama.info_imovel()
+
+    elif op==3:
+        menu=False
