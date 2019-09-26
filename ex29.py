c9ef2db4030c58428f5d4d21a6723e86b02ef6ee
andreboa-sorte <offesmecheplayer@gmail.com>
2019-09-26 19:53:26 -0300

    'ex29TxT'

---
+++ ex29.py
@@ -0,0 +1,8 @@
+'''29 - Qual a diferença de lista e dicionário?
+
+lista é um espaço de armazenamento de itens (acessa com base em index de N° inteiros)
+
+um dicionario é um uma especime de "lista" mostra a "tradução" do item, é baseado em
+chave -> valor
+
+'''
